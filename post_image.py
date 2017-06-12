07a5b64c8d0e6b4dc6b6237b26232d74ef5293b8
Juan Roberto Honorato <juanrhonorato@gmail.com>
2017-06-12 16:28:48 -0400

    change host ip

--- post_image.py
+++ post_image.py
@@ -2,8 +2,8 @@ import requests
 
 with open('0.jpg', 'rb') as f: 
     r = requests.post(
-            'http://localhost:8080/inference', 
-            #'http://13.92.198.82:8080/inference', 
+            #'http://localhost:8080/inference', 
+            'http://13.92.198.82:8080/inference', 
             files={'image': f}
         )
     print r.text
