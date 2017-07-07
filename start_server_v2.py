0963e1b235b36dba1fb3194729da7d44bb1cc879
Juan Roberto Honorato <juanrhonorato@gmail.com>
2017-07-07 13:08:39 -0400

    save recieved image to azure blob storage in parallel thread

--- start_server_v2.py
+++ start_server_v2.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from keras.models import load_model, model_from_json
 from bottle import BaseRequest, request, run, post, route
 from PIL import Image
@@ -10,10 +12,23 @@ from io import BytesIO
 import base64
 import urllib
 import json
-#from azure.storage.blob import ContentSettings
+import uuid # para generar nombre único, probablemente
+import threading
+from azure.storage.blob import BlockBlobService, ContentSettings
 
 # aumentar peso maximo de archivo a recibir, en bytes
 BaseRequest.MEMFILE_MAX = 1024 * 1024 * 10 # 10 MB max
+
+# para ejecutar funciones en hilos paralelos
+# en este caso guardar la imagen en azure blob storage
+class FuncThread(threading.Thread):
+    def __init__(self, target, *args):
+        self._target = target
+        self._args = args
+        threading.Thread.__init__(self)
+ 
+    def run(self):
+        self._target(*self._args)
 
 def square_image(im, new_side):
     old_size = im.size
@@ -44,62 +59,27 @@ def load_models(model_name):
 
     return loaded_model, graph
 
+def save_image_to_azure(base64_image, image_name):
+	'''
+	Guarda en Azure blob storage una imagen.
+	
+	:param str base64_image:
+		La imagen debe venir en base64 como string
+	'''
+	
+	block_blob_service = BlockBlobService(account_name='imagesanastasia', account_key='8WnRqr5RBPwaefcDyS60DTWqWX+nR02Bo4usikWn6UEttKxEUWnS8dy/0+z1hM4oAKToQOy9IjfCRjx7HZiO3A==')
+
+	block_blob_service.create_blob_from_text(
+		container_name = 'glucometros',
+		blob_name = image_name,
+		text = base64_image
+	)
+
+
 # cargar modelos en memoria
 global reg_model, clas_model, reg_graph, clas_graph  
 reg_model, reg_graph = load_models('regression')
 clas_model, clas_graph = load_models('classification')
-
-def save_image(image):
-    #block_blob_service.put_blob(
-    #    'glucometros',
-    #    'myblockblob',
-    #    'sunset.png',
-    #    content_settings=ContentSettings(content_type='image/png')
-    #)
-    pass
-    
-@post('/inference')
-def inference():
-    t0 = time()
-    data = request.files.image
-        
-    raw = data.file.read()
-    im = Image.open(StringIO(raw))
-    
-    # parametros a ocupar
-    GLUC_NEW_SIZE = 96
-    DIGITS_NEW_SIZE = 48
-        
-    # transformar imagen
-    im = square_image(im, new_side=GLUC_NEW_SIZE)
-    im = im.resize((GLUC_NEW_SIZE, GLUC_NEW_SIZE), resample=Image.BICUBIC)
-    
-    # pasar a formato que recibe el modelo, en numpy
-    image = np.expand_dims(np.array(im), axis=0) / 255.
-    
-    # obtener bbox
-    with reg_graph.as_default():
-	    regression = reg_model.predict(image)[0]
-	    x1, y1, x2, y2 = regression    
-    # recortar imagen con bbox y pasar a formato de siguiente modelo
-    dig = im.crop([x1, y1, x2, y2])
-    dig = square_image(dig, DIGITS_NEW_SIZE)
-    dig = dig.resize((DIGITS_NEW_SIZE, DIGITS_NEW_SIZE), resample=Image.BICUBIC)   
-        
-    digits = np.expand_dims(np.array(dig), axis=0) / 255.
-    
-    # finalmente encontrar digitos
-    with clas_graph.as_default():
-        classification = clas_model.predict(digits)
-        result = ''.join([str(np.argmax(y_)) if np.argmax(y_) != 10 else '' for y_ in classification])
-    response = {
-        'lectura': result,
-        'tiempo_inferencia': time() - t0,
-        'time_utc': strftime("%Y-%m-%d %H:%M:%S", gmtime()),
-        'storage': "entregar_id_unico_de_archivo"
-    }
-    
-    return response
 
 @post('/inferencev2')
 def inferencev2():
@@ -111,6 +91,11 @@ def inferencev2():
     jsonObj = json.loads(parsedBody)
     raw = jsonObj['image'] 
     raw = raw.replace('data:image/jpeg;base64,', '')
+
+	# guardar imagen en Azure en hilo paralelo
+    image_name = str(uuid.uuid4())
+    hilo_paralelo = FuncThread(save_image_to_azure, raw, image_name)
+    hilo_paralelo.start()
         
     im = Image.open(BytesIO(base64.b64decode(raw)))
     
@@ -140,11 +125,14 @@ def inferencev2():
     with clas_graph.as_default():
         classification = clas_model.predict(digits)
         result = ''.join([str(np.argmax(y_)) if np.argmax(y_) != 10 else '' for y_ in classification])
-    response = {
+	
+	hilo_paralelo.join()
+
+	response = {
         'lectura': result,
         'tiempo_inferencia': time() - t0,
         'time_utc': strftime("%Y-%m-%d %H:%M:%S", gmtime()),
-        'storage': "entregar_id_unico_de_archivo"
+        'storage': image_name
     }
     
     return response
